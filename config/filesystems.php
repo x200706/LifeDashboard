c0e2e4702f92ab3178dfef7bd0b5a60fd83eada4
Crystal <chi200706@gmail.com>
2024-05-01 21:08:38 +0800

    修改S3設定

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,6 +51,8 @@ return [
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'visibility' => 'public',
+            'bucket_endpoint' => false,
         ],
         'admin' => [
             'driver'     => 'local',
