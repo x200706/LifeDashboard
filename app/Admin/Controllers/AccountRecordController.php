fdfeb762f10b1630508c90a26364955253338cfb
Crystal <chi200706@gmail.com>
2024-05-23 22:30:54 +0800

    改良顯示字樣提升使用者體驗

--- app/Admin/Controllers/AccountRecordController.php
+++ app/Admin/Controllers/AccountRecordController.php
@@ -96,7 +96,7 @@ class AccountRecordController extends AdminController
         // 但已知有個送出提示按鈕卡住的bug orz
         $form->date('date', '日期')->rules('required');
         $form->text('name', '名稱')->rules('required|min:1');
-        $form->select('type', '收支類型')->options(['income' => '收入','expense' => '支出',])->rules('required');
+        $form->select('type', '收支類型')->options(['income' => '增加','expense' => '減少',])->rules('required');
         $form->select('tag', '記帳分類')->options(AccountRecordTags::all()->pluck('desc','name'))->rules('required');
         $form->number('amount', '金額')->rules('required|regex:/^[1-9][0-9]*$/|min:1');
         $form->select('account', '帳戶')->options(Account::all()->pluck('desc','name'))->rules('required');
